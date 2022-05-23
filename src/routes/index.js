35dcdf0a53ef15da0ca765dee0c3070dc788695a
tanxcypro <58644969+tanxcypro@users.noreply.github.com>
2022-05-23 17:34:23 +0700

    update

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,10 +1,29 @@
 
 //instantiate express module here
+const express=require('express')
 
 // Init express router here..
+const router=express.Router()
 
 // Get controller here
+const {
+  getTodos,
+  getTodo,
+  addTodo,
+  updateTodo,
+  deleteTodo,
+} = require("../controllers/todo");
 
 // Create Route here
 
+
+// Route
+router.get("/todos", getTodos);
+router.get("/todo/:id", getTodo);
+router.post("/todo", addTodo);
+router.patch("/todo/:id", updateTodo);
+router.delete("/todo/:id", deleteTodo);
+
+
 // Export module router here
+module.exports=router
